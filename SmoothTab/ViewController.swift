04aa4bbca7b2ef2e4bd32198532bcd99d390a954
Yervand Saribekyan <yervand.saribekyan.stdev@gmail.com>
2018-03-02 10:36:27 +0400

    Initial Commit

---
+++ SmoothTab/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SmoothTab
+//
+//  Created by Yervand Saribekyan on 3/2/18.
+//  Copyright © 2018 Yervand Saribekyan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
